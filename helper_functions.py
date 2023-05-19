edf8fe74a65c8d72d0552bb22066f7914123a7f1
James Davey <58090981+Molten-Ice@users.noreply.github.com>
2023-05-19 17:10:23 +0100

    Add files via upload

--- helper_functions.py
+++ helper_functions.py
@@ -90,7 +90,7 @@ def poisson_blend(paste_image, source_img, box):
 
 ##### Inpainting #####
 
-def train(fixed_noise, cropped_real_face_tensor, mask, lr = 0.0003, iterations = 1500, lam = 0.1, eval_interval = 200):
+def train(G, D, fixed_noise, cropped_real_face_tensor, mask, lr = 0.0003, iterations = 1500, lam = 0.1, eval_interval = 200):
 
     #lam is perceptual_loss factor
     progress = []
@@ -122,7 +122,7 @@ def tensor_to_np(img):
     torch_grid = torchvision.utils.make_grid(img.cpu(), normalize = True, padding = 0)
     return np.ascontiguousarray((torch_grid.permute(1,2,0).numpy()*255), dtype=np.uint8)
 
-def inpaint(G, mtcnn, device, cropped_face, fixed_noise, lr = 0.0003, iterations = 1500, lam = 0.1, eval_interval = 200, display_intermediate = False):
+def inpaint(G, D, mtcnn, device, cropped_face, fixed_noise, lr = 0.0003, iterations = 1500, lam = 0.1, eval_interval = 200, display_intermediate = False):
 
     ## Generate image             
     generated_img_tensor = G(fixed_noise, None)  # NCHW, float32, dynamic range [-1, +1], None is class labels
@@ -161,7 +161,7 @@ def inpaint(G, mtcnn, device, cropped_face, fixed_noise, lr = 0.0003, iterations
         display(ImageOps.contain(Image.fromarray(display_img), (1500, 1500)))
 
     ## Train ##
-    progress = train(fixed_noise, cropped_real_face_tensor, mask, lr = lr, iterations = iterations, lam = lam, eval_interval = eval_interval)
+    progress = train(G, D, fixed_noise, cropped_real_face_tensor, mask, lr = lr, iterations = iterations, lam = lam, eval_interval = eval_interval)
 
     if display_intermediate:
         images_to_concat = []
